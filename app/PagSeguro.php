75bbecab4981b6b84d19bfa285293a06be53ae0e
Diogo Cezar <diogo@diogocezar.com>
2016-09-15 14:54:45 -0300

    Notifications

--- app/PagSeguro.php
+++ app/PagSeguro.php
@@ -134,7 +134,8 @@ class CreatePaymentRequest {
     }
 }
 
-class NotificationListener {
+//class NotificationListener {
+    /*
     public static function main(){
         print_r($_POST);
         if(isset($_POST['notificationCode']) && $_POST['notificationType'] == 'transaction'){
@@ -155,5 +156,104 @@ class NotificationListener {
             $transaction = simplexml_load_string($transaction);
             print_r($transaction);
         }
+    }
+    */
+class NotificationListener
+{
+    public function main()
+    {
+
+        $code = (isset($_POST['notificationCode']) && trim($_POST['notificationCode']) !== "" ?
+            trim($_POST['notificationCode']) : null);
+        $type = (isset($_POST['notificationType']) && trim($_POST['notificationType']) !== "" ?
+            trim($_POST['notificationType']) : null);
+
+        if ($code && $type) {
+
+            $notificationType = new PagSeguroNotificationType($type);
+            $strType = $notificationType->getTypeFromValue();
+
+            switch ($strType) {
+
+                case 'TRANSACTION':
+                    self::transactionNotification($code);
+                    break;
+
+                case 'APPLICATION_AUTHORIZATION':
+                    self::authorizationNotification($code);
+                    break;
+
+                case 'PRE_APPROVAL':
+                    self::preApprovalNotification($code);
+                    break;
+
+                default:
+                    LogPagSeguro::error("Unknown notification type [" . $notificationType->getValue() . "]");
+
+            }
+
+            self::printLog($strType);
+
+        } else {
+
+            LogPagSeguro::error("Invalid notification parameters.");
+            self::printLog();
+
+        }
+
+    }
+
+    private static function transactionNotification($notificationCode)
+    {
+
+        $credentials = PagSeguroConfig::getAccountCredentials();
+
+        try {
+            $transaction = PagSeguroNotificationService::checkTransaction($credentials, $notificationCode);
+            echo "passou";
+            print_r($transaction);
+            // Do something with $transaction
+        } catch (PagSeguroServiceException $e) {
+            die($e->getMessage());
+        }
+    }
+
+    private static function authorizationNotification($notificationCode)
+    {
+
+        $credentials = PagSeguroConfig::getApplicationCredentials();
+
+        try {
+            $authorization = PagSeguroNotificationService::checkAuthorization($credentials, $notificationCode);
+
+            // Do something with $authorization
+        } catch (PagSeguroServiceException $e) {
+            die($e->getMessage());
+        }
+    }
+
+    private static function preApprovalNotification($preApprovalCode)
+    {
+
+        $credentials = PagSeguroConfig::getAccountCredentials();
+
+        try {
+            $preApproval = PagSeguroNotificationService::checkPreApproval($credentials, $preApprovalCode);
+            // Do something with $preApproval
+
+        } catch (PagSeguroServiceException $e) {
+            die($e->getMessage());
+        }
+    }
+
+    private static function printLog($strType = null)
+    {
+        $count = 4;
+        echo "<h2>Receive notifications</h2>";
+        if ($strType) {
+            echo "<h4>notifcationType: $strType</h4>";
+        }
+        echo "<p>Last <strong>$count</strong> items in <strong>log file:</strong></p><hr>";
+        echo LogPagSeguro::getHtml($count);
     }
 }
