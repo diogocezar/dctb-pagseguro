8dc7a06b7df0d33428f2ca1901be32eb6dc25027
Diogo Cezar <diogo@diogocezar.com>
2016-09-15 14:28:18 -0300

    Ajustes Notification

--- notification.php
+++ notification.php
@@ -1,5 +1,6 @@
 <h1>Houve uma alteração no status</h1>
 <?php
+	header("access-control-allow-origin: https://sandbox.pagseguro.uol.com.br");
 	require_once('./app/autoload.php');
 	$nl = new NotificationListener();
 	$nl->main();
